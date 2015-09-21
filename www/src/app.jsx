e41206eec4492f6ae01b5bcd6a6a0d530033713c
Cole R Lawrence <cole@zombiehippie.com>
2015-09-21 13:43:35 -0500

    Add links to course details pages

--- www/src/app.jsx
+++ www/src/app.jsx
@@ -57,6 +57,8 @@ catalogApp.config( ($routeProvider) => {
 .controller('CourseViewCtrl', ($scope, $routeParams, HoverInfo) => {
   $scope.$on('$routeChangeSuccess', function (event, current, previous) {
     $scope.courseId = current.pathParams.courseId
+    var avail = "https://prodssb.missouristate.edu/prod/bwckctlg.p_disp_listcrse?term_in=201620&subj_in=$1&crse_in=$2&schd_in="
+    $scope.availabilityIframe = $scope.courseId.replace(/([A-Z]+) ?(\\d+)/, avail)
     gocoursesort.ready(()=> {
       gocoursesort.get($scope.courseId, (error, course)=> {
         $scope.error = error
